0bb27b885296351e5a14341ba0bbf6a8b94d19ff
Martelka23 <isupov.2001@mail.ru>
2023-06-20 09:39:27 +0300

    Test schema to params

--- src/utils/SwaggerRequest.ts
+++ src/utils/SwaggerRequest.ts
@@ -1,6 +1,7 @@
 import { 
     HttpStatusCode, 
     HttpStatusNumber, 
+    ISwaggerSchema, 
     SwaggerBodyContentTypes, 
     SwaggerPathRequest, 
     SwaggerRequestParameter 
@@ -32,7 +33,7 @@ export class SwaggerRequest {
         return this;
     }
 
-    addParam(param: SwaggerRequestParameter | string): SwaggerRequest {
+    addParam(param: SwaggerRequestParameter): SwaggerRequest {
         if (!this.request.parameters) {
             this.request.parameters = [];
         }
@@ -50,8 +51,33 @@ export class SwaggerRequest {
         return this;
     }
 
-    addParams(params: Array<SwaggerRequestParameter | string>): SwaggerRequest {
-        params.forEach(param => this.addParam(param));
+    addParams(params: SwaggerRequestParameter[], schema?: ISwaggerSchema): SwaggerRequest {
+        // const _params: SwaggerRequestParameter[] = [];
+
+        // const schemaKeys = Object.keys(schema.properties);
+
+        // schemaKeys.forEach(key => {
+        //     _params.push({ name: key, example: schema.properties[key], in: 'query' });
+        // });
+
+        // params.forEach(param => {
+        //     const index = _params.findIndex(el => el.name === param.name);
+        //     if (index >= 0) {
+        //         _params[index] = { ..._params[index], ...param };
+        //     }
+        // });
+
+        params.forEach(param => {
+            const _param: SwaggerRequestParameter = schema ? { 
+                name: param.name,
+                example: schema.properties[param.name].example,
+                required: schema.required ? schema.required.includes(param.name) : undefined,
+                in: 'query',
+                ...param
+            } : { ...param };
+
+            this.addParam(_param);
+        });
 
         return this;
     }
@@ -83,9 +109,29 @@ export class SwaggerRequest {
         return this;
     }
 
-    
-
     getConfig() {
         return this.request;
     }
 }
+
+const test = {
+    type: 'object',
+    properties: {
+      id: { type: 'number', format: 'float', minimum: 1, example: 1 },
+      email: { type: 'string', format: 'email', example: 'test@test.ru' },
+      phone: { type: 'string', example: '79123456789' },
+      companyName: { type: 'string', example: 'My company' },
+      juristicName: { type: 'string', example: 'OAO My company' },
+      contactPreferenceId: { type: 'number', format: 'float', minimum: 1, example: 1 },
+      requestStatusId: { type: 'number', format: 'float', minimum: 1, example: 1 }
+    },
+    required: [ 'id' ],
+    additionalProperties: false
+  };
+
+
+const swaggerRequest = new SwaggerRequest();
+
+swaggerRequest.addParams([], test)
+
+console.log(swaggerRequest.getConfig())
